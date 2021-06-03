8d013551d6873cd112f5005f855df8f928603a02
bikashdey <74043649+bikashdey@users.noreply.github.com>
2021-06-03 18:40:50 +0530

    Update pie&bar_json.py

--- pie&bar_json.py
+++ pie&bar_json.py
@@ -6,31 +6,25 @@ import json
 #df = pd.read_json('json_data')
 
 
-
 with open('json_data','r') as f:
-	#df_list = pd.DataFrame(json.load(f))
-	df = pd.DataFrame.from_dict(f, orient='index')
-
-
+	df_list = pd.DataFrame(json.load(f))
+	
 print(df)
-df.plot(kind='bar')
-df.plot.pie(subplots=True)
 
-
-"""
 x = df['Name']
 y = df['Call']
 z= df['Duration']
 
 #for pie chart.....
 
-plt.pie(y,labels=x,radius=1.2,autopct='%0.02f%%',shadow=True,explode = [.05,.2,.05,.2,.05,.2])
+df.plot.pie(subplots=True)
+#plt.pie(y,labels=x,radius=1.2,autopct='%0.02f%%',shadow=True,explode = [.05,.2,.05,.2,.05,.2])
 
 #for bar chart ..
 
 plt.xlabel('Name',fontsize=18)
 plt.ylabel('Duration',fontsize=16)
-plt.bar(x,z)"""
+plt.bar(x,z)
 
 
 plt.show()
